ce575614dd0db87ec743215afe150776fd72d6aa
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2020-03-13 15:02:36 +0700

    add listView and floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -24,24 +23,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
+  // final List<String> entries = <String>['A', 'B', 'C'];
+  // final List<int> colorCodes = <int>[600, 500, 100];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(
-          children: const <Widget>[
-            Card(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemCount: 15,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
               child: ListTile(
                 leading: FlutterLogo(size: 72.0),
                 title: Text('wayan edi sudarma'),
                 subtitle: Text('081311902630989895656'),
               ),
-            ),
-          ],
-        ));
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
