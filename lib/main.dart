f8e152cd5cea430ed821c0da127dda34c873bccc
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2020-03-13 15:50:20 +0700

    add page route

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:contact/pages/MyHomePage.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,48 +6,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    var materialApp = MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Contact'),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => MyHomePage(),
+      },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // final List<String> entries = <String>['A', 'B', 'C'];
-  // final List<int> colorCodes = <int>[600, 500, 100];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-          itemCount: 15,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: ListTile(
-                leading: FlutterLogo(size: 72.0),
-                title: Text('wayan edi sudarma'),
-                subtitle: Text('081311902630989895656'),
-              ),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+    return materialApp;
   }
 }
---
+++ lib/pages/MyHomePage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // final List<String> entries = <String>['A', 'B', 'C'];
+  // final List<int> colorCodes = <int>[600, 500, 100];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contacts"),
+      ),
+      body: ListView.builder(
+          itemCount: 15,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 72.0),
+                title: Text('wayan edi sudarma'),
+                subtitle: Text('081311902630989895656'),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
